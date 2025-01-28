c47991e7f0d7be10578786a4e7b1d71cdfc87e46
Saket Agarwal <129582257+SaketAgarwal007@users.noreply.github.com>
2025-01-29 02:55:26 +0530

    config updated

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-const port = process.env.PORT || 5174;
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+// vite.config.js
+export default {
+  server: {
+    port: 8080,  // Render expects port 8080
+    host: '0.0.0.0',  // This ensures that it listens on all network interfaces
+  }
+}
