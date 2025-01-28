1e7a3fbac5d3297a530e9338bc5ceff90d2793ee
Saket Agarwal <129582257+SaketAgarwal007@users.noreply.github.com>
2025-01-29 03:16:48 +0530

    bug fixed

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-const port = process.env.PORT || 5173;
+const port = process.env.PORT || 5175;
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
