cddbfdfa12a0f3baec59aa5149616b013c5d272e
Saket Agarwal <129582257+SaketAgarwal007@users.noreply.github.com>
2025-01-29 03:21:02 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 const port = process.env.PORT || 5175;
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    allowedHosts: ['hitpredict.onrender.com']
+  },
   plugins: [react()],
 })
